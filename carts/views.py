8cc6b6f2436109064c28f28177bf853538987761
leprikon8888 <leprikon8888@gmail.com>
2024-03-08 22:47:15 +0200

    add to cart ajax

--- carts/views.py
+++ carts/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render, redirect
+from django.http import JsonResponse
+from django.shortcuts import redirect
+from django.template.loader import render_to_string
 
 from carts.models import Cart
+from carts.utils import get_user_carts
 from goods.models import Products
 
 
-def cart_add(request, product_slug):
+def cart_add(request):
 
-    product = Products.objects.get(slug=product_slug)
+    product_id = request.POST.get('product_id')
+    product = Products.objects.get(id=product_id)
 
     if request.user.is_authenticated:
         carts = Cart.objects.filter(user=request.user, product=product)
@@ -14,12 +18,21 @@ def cart_add(request, product_slug):
         if carts.exists():
             cart = carts.first()
             if cart:
-                cart.quantity +=1
+                cart.quantity += 1
                 cart.save()
         else:
             Cart.objects.create(user=request.user, product=product, quantity=1)
 
-    return redirect(request.META['HTTP_REFERER'])
+    user_cart = get_user_carts(request)
+    cart_items_html = render_to_string(
+        'carts/includes/included_cart.html', {'carts': user_cart}, request=request)
+
+    response_data = {
+        'message': 'Товар додан до кошика',
+        'cart_items_html': cart_items_html,
+    }
+
+    return JsonResponse(response_data)
 
 
 def cart_change(request, product_slug):
